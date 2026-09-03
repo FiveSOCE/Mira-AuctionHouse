6f952542ba4861abcd0c58cf60c3f82c750316f2
FiveS <sgmmsionide@gmail.com>
2026-09-03 20:57:15 +1000

    Wire Auction House market analytics

--- src/main/java/com/mira/auctionhouse/MiraAuctionHousePlugin.java
+++ src/main/java/com/mira/auctionhouse/MiraAuctionHousePlugin.java
@@ -2,7 +2,9 @@ package com.mira.auctionhouse;
 
 import com.mira.auctionhouse.command.AuctionCommand;
 import com.mira.auctionhouse.gui.AuctionGuiService;
+import com.mira.auctionhouse.listener.AuctionAnalyticsCommandListener;
 import com.mira.auctionhouse.listener.AuctionListener;
+import com.mira.auctionhouse.service.AuctionAnalyticsService;
 import com.mira.auctionhouse.service.AuctionService;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.milkbowl.vault.economy.Economy;
@@ -29,22 +31,21 @@ public final class MiraAuctionHousePlugin extends JavaPlugin {
 
         auctions = new AuctionService(this, economy);
         gui = new AuctionGuiService(this, auctions);
+        AuctionAnalyticsService analytics = new AuctionAnalyticsService(this);
         AuctionCommand command = new AuctionCommand(this, auctions, gui);
         PluginCommand pluginCommand = getCommand("auctionhouse");
         if (pluginCommand == null) throw new IllegalStateException("auctionhouse command missing from plugin.yml");
         pluginCommand.setExecutor(command);
         pluginCommand.setTabCompleter(command);
         getServer().getPluginManager().registerEvents(new AuctionListener(this, auctions, gui), this);
+        getServer().getPluginManager().registerEvents(new AuctionAnalyticsCommandListener(this, analytics), this);
 
         long period = Math.max(20L, getConfig().getLong("expiry-check-seconds", 60L) * 20L);
         getServer().getScheduler().runTaskTimer(this, auctions::expireNow, period, period);
-        getLogger().info("MiraAuctionHouse v" + getPluginMeta().getVersion() + " enabled.");
+        getLogger().info("MiraAuctionHouse v" + getPluginMeta().getVersion() + " enabled with market price analytics.");
     }
 
-    public void reloadAll() {
-        reloadConfig();
-        auctions.reload();
-    }
+    public void reloadAll() { reloadConfig(); auctions.reload(); }
 
     public void msg(CommandSender sender, String message) {
         String prefix = getConfig().getString("messages.prefix", "&5[AH] &r");
